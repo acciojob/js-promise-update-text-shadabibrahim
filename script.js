3aa689c9c4eb996f2b7dcc2d7a110be058608b27
Mohd Shadab <89182232+shadabibrahim@users.noreply.github.com>
2023-06-28 15:05:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+let out = document.querySelector("#id");
+	let data = new Promise((resolve,reject)=>{
+		setTimeOut(()=>{
+			resolve("Hello,world!")
+		},1000)
+	})
+
+data.then((item)=>{
+	out.innerHTML = item;
+})
+
